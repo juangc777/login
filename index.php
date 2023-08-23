5e8846ae538ef7ff28bdd20847a07ab94b7e7124
juangc777 <142953456+juangc777@users.noreply.github.com>
2023-08-23 16:29:27 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+    
+</head>
+<body>
+   <form action="validar.php" method="post">
+   <h1 class="animate__animated animate__backInLeft">Sistema de login</h1>
+   <p>Usuario <input type="text" placeholder="ingrese su nombre" name="usuario" id="usuario"></p>
+   <p>Contraseña <input type="password" placeholder="ingrese su contraseña" name="contraseña" id="contraseña"></p>
+   <input type="submit" value="Ingresar">
+   
+   </form> 
+</body>
+</html>
